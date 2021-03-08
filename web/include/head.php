6617baedb981f7844a270f305ac7edc462f69c04
Silvan <silvan@fuxli.ch>
2021-03-08 03:52:26 +0100

    Created player test page.

---
+++ web/include/head.php
@@ -0,0 +1,23 @@
+<meta charset="utf-8">
+
+<?php
+  echo "<title> " . $htmlTitle . " </title>";
+?>
+
+<?php /* Fonts */ ?>
+<link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
+
+
+<?php /* Player */ ?>
+<link href="node_modules/video.js/dist/video-js.min.css" rel="stylesheet">
+<script src="node_modules/video.js/dist/video.min.js"></script>
+<script src="node_modules/videojs-hotkeys/videojs.hotkeys.min.js"></script>
+
+
+<?php /* own */ ?>
+<link href="css/main.css" rel="stylesheet">
+<link href="css/player.css" rel="stylesheet">
+<script defer src="js/player.js"></script>
---
+++ web/index.php
@@ -0,0 +1,21 @@
+<?php
+  $htmlTitle = "Player Test";
+?>
+
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+  <head>
+    <base href="/wedeo.me/web/">
+    <?php require_once("include/head.php"); ?>
+  </head>
+  <body>
+    <video
+      id="wedeo-player"
+      class="wedeo-player video-js"
+      controls
+      poster="https://www.we-teve.com/images/thumb/large_img/ZL7CM0Rd.jpg"
+    >
+      <source src="https://www.we-teve.com/videos/JnhdhWTGOaCFMzPPAca0JkDyW/1080p.mp4" type="video/mp4" />
+    </video>
+  </body>
+</html>
