b3a79d2c7a2bdc0028ccb58824110bea67d635ac
Silinator <silvan@fuxli.ch>
2021-03-12 03:29:40 +0100

    Added Player modification who makes the video seeking smooth.
    From: https://github.com/videojs/video.js/issues/4460

--- web/js/player.js
+++ web/js/player.js
@@ -1,3 +1,6 @@
+/* makes seeking smooth */
+{
+  const SeekBar = videojs.getComponent('SeekBar')
 
 var Player = videojs('wedeo-player', {
   poster: 'https://www.we-teve.com/images/thumb/large_img/ZL7CM0Rd.jpg',
@@ -40,5 +43,27 @@ function ChangeSource() {
     setTimeout( function() {
         Player.pause();
     }, 150); /* how long it usually takes to play the video to not interrupt that */
+  SeekBar.prototype.getPercent = function getPercent() {
+    // Allows for smooth scrubbing, when player can't keep up.
+    const time = this.player_.currentTime();
+    const percent = time / this.player_.duration();
+    return percent >= 1 ? 1 : percent;
+  }
+
+  SeekBar.prototype.handleMouseMove = function handleMouseMove(event) {
+    let newTime = this.calculateDistance(event) * this.player_.duration();
+
+    if( newTime === this.player_.duration() ) {
+      newTime = newTime - 0.1;
+    }
+
+    this.player_.currentTime(newTime);
+    this.update();
+  }
+}
+
+/******/
+
+
   }
 }
