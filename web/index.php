26d6842831958614c8a97fb2cd1c2128a988c919
Silinator <silvan@fuxli.ch>
2021-03-12 03:32:44 +0100

    Moved the player logic into the class wedeoPlayerClass. Changed autoplay option from true to "any".

--- web/index.php
+++ web/index.php
@@ -9,9 +9,16 @@
     <?php require_once("include/head.php"); ?>
   </head>
   <body>
-    <div class="videoContainer">
-      <video id="wedeo-player" class="wedeo-player video-js" controls></video>
+    <!-- <div class="videoContainer"> -->
+    <div class="videoContainer-bg">
+      <!-- <video id="wedeo-player" class="wedeo-player video-js" controls></video> -->
+      <video id="wedeo-player" class="wedeo-bg-player video-js" controls loop></video>
     </div>
-    <button onClick="ChangeSource()">KLICK</button>
+    <button onClick="wedeoPlayer.changeSource( '240p.mp4' )">240p</button>
+    <script type="text/javascript">
+      var defaultResult = "1080p.mp4";
+      var wedeoPlayer = new wedeoPlayerClass( 'wedeo-player', defaultResult );
+      wedeoPlayer.setVideo( "ZL7CM0Rd", "JnhdhWTGOaCFMzPPAca0JkDyW" );
+    </script>
   </body>
 </html>
--- web/js/player.js
+++ web/js/player.js
@@ -2,47 +2,6 @@
 {
   const SeekBar = videojs.getComponent('SeekBar')
 
-var Player = videojs('wedeo-player', {
-  poster: 'https://www.we-teve.com/images/thumb/large_img/ZL7CM0Rd.jpg',
-  sources: [{
-    src: 'https://www.we-teve.com/videos/JnhdhWTGOaCFMzPPAca0JkDyW/1080p.mp4',
-    type: 'video/mp4'
-  }],
-  playbackRates: [0.5, 1, 1.5, 2],
-  preload: true,
-  inactivityTimeout: 700,
-  autoplay: true
-});
-
-Player.ready(function() {
-  this.hotkeys({
-    volumeStep: 0.1,
-    seekStep: 5,
-    enableModifiersForNumbers: false
-  });
-});
-
-function ChangeSource() {
-  const currentTime = Player.currentTime();
-  const videoIsPlaused = Player.paused();
-  const isVideoMuted = Player.muted();
-
-  Player.muted(true);
-  Player.pause();
-  Player.src( "https://www.we-teve.com/videos/rbhAs49EhXbxNgW8QJNkbdJcW/1080p.mp4" );
-
-  Player.load();
-  Player.pause();
-
-  Player.currentTime( currentTime );
-  Player.play();
-
-  if( !isVideoMuted ){ Player.muted(false) };
-
-  if( videoIsPlaused ) {
-    setTimeout( function() {
-        Player.pause();
-    }, 150); /* how long it usually takes to play the video to not interrupt that */
   SeekBar.prototype.getPercent = function getPercent() {
     // Allows for smooth scrubbing, when player can't keep up.
     const time = this.player_.currentTime();
@@ -65,5 +24,97 @@ function ChangeSource() {
 /******/
 
 
+class wedeoPlayerClass {
+  constructor( playerId, defaultResolution ) {
+    this.URLbase      = "https://www.we-teve.com/";
+    this.playerId     = playerId;
+    this.defaultRes   = defaultResolution;
+
+    this.createPlayer();
+  }
+
+  createPlayer() {
+    const self = this;
+
+    this.Player = videojs( this.playerId, {
+      preload: true,
+      inactivityTimeout: 1000,
+      autoplay: "any"
+    });
+
+    if( $("#" + self.playerId).hasClass("wedeo-bg-player") ) {
+      self.playerType = "background";
+      self.createBackgroundPlayer();
+    } else {
+      self.playerType = "default";
+      self.createDefaultPlayer();
+    }
+  }
+
+  createDefaultPlayer() {
+    this.addHotkeys();
+  }
+
+  createBackgroundPlayer() {
+    const self = this;
+    $("#" + this.playerId).append( "<div class='backgrpund-player-mute-btn'></div>" );
+    $("#" + this.playerId  + " .backgrpund-player-mute-btn").click( function(){
+      self.Player.muted(!self.Player.muted());
+    });
+
+    setTimeout(function () { self.Player.volume(  0.5 ); }, 0);
+  }
+
+  addHotkeys() {
+    this.Player.ready(function() {
+      this.hotkeys({
+        volumeStep: 0.1,
+        seekStep: 5,
+        enableModifiersForNumbers: false
+      });
+    });
+  }
+
+  setVideo( vuid, datavuid ) {
+    this.vuid = vuid;
+    this.datavuid = datavuid;
+
+    this.media = {
+      poster: this.URLbase + '/images/thumb/large_img/' + this.vuid + '.jpg',
+      src: this.URLbase + '/videos/' + this.datavuid + '/' + this.defaultRes
+    };
+
+    this.Player.loadMedia(this.media);
+  }
+
+  changeSource( resolution ) {
+    this.defaultRes = resolution;
+    const p = this.Player;
+    const currentTime = p.currentTime();
+    const videoIsPlaused = p.paused();
+    const isVideoMuted = p.muted();
+
+    p.muted(true);
+    p.pause();
+    p.src( this.URLbase + '/videos/' + this.datavuid  + '/' + this.defaultRes );
+
+    p.load();
+    p.pause();
+
+    p.currentTime( currentTime );
+    p.play();
+
+    if( !isVideoMuted ){ p.muted(false) };
+
+    if( videoIsPlaused ) {
+      setTimeout( function() {
+        p.pause();
+      }, 150); /* how long it usually takes to play the video to not interrupt that */
+    }
+  }
+
+  changePlayback( playbackRate ) {
+    this.playbackRate = playbackRate;
+    this.Player.playbackRate(this.playbackRate);
   }
 }
