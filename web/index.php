4d8b2a221186b8fadc35ddafa175acfa0c13db76
Silinator <silvan@fuxli.ch>
2021-03-12 03:33:11 +0100

    Added more test buttons.

--- web/index.php
+++ web/index.php
@@ -15,6 +15,12 @@
       <video id="wedeo-player" class="wedeo-bg-player video-js" controls loop></video>
     </div>
     <button onClick="wedeoPlayer.changeSource( '240p.mp4' )">240p</button>
+    <button onClick="wedeoPlayer.changeSource( '480p.mp4' )">480p</button>
+    <button onClick="wedeoPlayer.changeSource( '1080p.mp4' )">1080p</button>
+    <button onClick="wedeoPlayer.changeSource( 'audio.mp3' )">Audio</button>
+    <br/>
+    <button onClick="wedeoPlayer.changePlayback( 2 )">KLICK2</button>
+
     <script type="text/javascript">
       var defaultResult = "1080p.mp4";
       var wedeoPlayer = new wedeoPlayerClass( 'wedeo-player', defaultResult );
