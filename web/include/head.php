0e9b82cce215ab3d180b35b5ff33beefe76c6af5
Silinator <silvan@fuxli.ch>
2021-03-12 03:22:02 +0100

    Added jquery.

--- web/include/head.php
+++ web/include/head.php
@@ -10,6 +10,9 @@
 <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
 
+<?php /* jQuery */ ?>
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
+
 
 <?php /* Player */ ?>
 <link href="node_modules/video.js/dist/video-js.min.css" rel="stylesheet">
