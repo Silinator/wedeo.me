89926d6ca489098ac416004664e671b427135525
Silinator <silvan@fuxli.ch>
2021-03-19 04:29:26 +0100

    Renamed videoContainer to wedeoContainer.

--- web/index.php
+++ web/index.php
@@ -9,11 +9,11 @@
     <?php require_once("include/head.php"); ?>
   </head>
   <body>
-    <videoContainer>
+    <wedeoContainer>
     <!-- <div class="videoContainer-bg"> -->
       <video id="wedeo-player" class="wedeo-player video-js" controls></video>
       <!-- <video id="wedeo-player" class="wedeo-bg-player video-js" controls></video> -->
-    </videoContainer>
+    </wedeoContainer>
 
     <script type="text/javascript">
       const availableSources = [ "audio", "240p", "480p", "1080p" ];
