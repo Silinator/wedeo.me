43a178a06e0c45a6417364d8006d0fc20db93613
Silinator <silvan@fuxli.ch>
2021-03-20 02:13:18 +0100

    Added own icon font.

--- web/include/head.php
+++ web/include/head.php
@@ -4,17 +4,20 @@
   echo "<title> " . $htmlTitle . " </title>";
 ?>
 
-<?php /* Fonts */ ?>
+<?php /* fonts */ ?>
 <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
 
+<?php /* own icon font */ ?>
+<link rel="stylesheet" href="font/icon-font/style.css"></head>
+
 <?php /* jQuery */ ?>
 <script src="node_modules/jquery/dist/jquery.min.js"></script>
 
 
-<?php /* Player */ ?>
+<?php /* wedeo player */ ?>
 <link href="node_modules/video.js/dist/video-js.min.css" rel="stylesheet">
 <script src="node_modules/video.js/dist/video.min.js"></script>
 <script src="node_modules/videojs-hotkeys/videojs.hotkeys.min.js"></script>
