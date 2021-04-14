e06539b56214b907d4cc1fb87aaa3346f79b1662
Silinator <silvan@fuxli.ch>
2021-04-15 00:04:42 +0200

    Added fast filter buttons to more videos tab.

--- web/js/vue/components/wedeoSideMoreVideos.js
+++ web/js/vue/components/wedeoSideMoreVideos.js
@@ -7,21 +7,42 @@ Vue.component( 'wedeoSideMoreVideos', {
   },
   template: `
     <div class='wedeoSideMoreVideos'>
-			More Videos
+			<div class='wedeoSideMoreVideosCatContainer'>
+				<div v-on:click="applyCatFilter(videoInfo.category)" class='outlineBtn' :title="videoCategoryFull">{{videoCategoryFull}}</div>
+				<div v-on:click="applyLangFilter(videoInfo.language)" class='outlineBtn' :title="videoLanguageFull">{{videoLanguageFull}}</div>
+				<div v-on:click="applyUserFilter(videoInfo.user.name)" class='outlineBtn' :title="videoInfo.user.name">{{videoInfo.user.name}}</div>
+				<div v-on:click="apllyTagFilter(tag)" v-for="tag in tags" class='outlineBtn' :title="tag">{{tag}}</div>
+			</div>
     </div>
   `,
   computed: {
 		videoInfo() {
 			return this.$store.state.currentVideoInfo;
+		},
+		videoCategoryFull() {
+			return t('CAT_' + this.videoInfo.category.toUpperCase() );
+		},
+		videoLanguageFull() {
+			return t('LANG_' + this.videoInfo.language.toUpperCase() );
+		},
+		tags() {
+			let tags = this.videoInfo.tags.split(", ");
+			return tags.filter( tag => { return tag.trim() != ""; });
 		}
   },
   methods: {
-    changeActiveTab( tab ) {
-      this.activeTab = tab;
+    applyCatFilter(cat) {
+	    //stuff
     },
-    headerBtnClass( tab ) {
-      return "wedeoSideContainerHeaderBtn noSel " + ( tab == this.activeTab ? "active" : "" );
-    }
+    applyLangFilter(lang) {
+      //stuff
+    },
+		applyUserFilter(user) {
+			//stuff
+		},
+		applyTagFilter(tag) {
+			//stuff
+		}
   },
   mounted() {
   }
