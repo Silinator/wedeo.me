82847f310d90cc41be8032d1066814f651042799
Silinator <silvan@fuxli.ch>
2021-04-14 03:17:56 +0200

    Added short video info.

--- web/js/vue/components/wedeoSideInfo.js
+++ web/js/vue/components/wedeoSideInfo.js
@@ -8,13 +8,23 @@ Vue.component( 'wedeoSideInfo', {
     <div class='wedeoSideInfo'>
 			<h2>{{t('PUBLISHED_ON', this.uploadDate)}}</h2>
 			<userPreview :userInfo="videoInfo.user"/>
-      <div class="wedeoSideInfoDescription" v-html="description">
+      <div class="wedeoSideInfoDescription" v-html="description"></div>
+      <div class="wedeoSideShortInfo">
+        <div class="wedeoSideShortInfoTitle">{{t('CAT_MAIN')}}:</div> <div>{{videoCategoryFull}}</div>
+        <div class="wedeoSideShortInfoTitle">{{t('LANG_MAIN')}}:</div> <div>{{videoLanguageFull}}</div>
+      </div>
     </div>
   `,
   computed: {
 		videoInfo() {
 			return this.$store.state.currentVideoInfo;
 		},
+    videoCategoryFull() {
+      return t('CAT_' + this.videoInfo.category.toUpperCase() );
+    },
+    videoLanguageFull() {
+      return t('LANG_' + this.videoInfo.language.toUpperCase() );
+    },
 		uploadDate() {
 			return moment.unix(this.videoInfo.publishDate).format("DD.MM.YYYY");
 		},
