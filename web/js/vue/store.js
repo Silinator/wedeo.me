b5772e0e170ec36d8657bd7f0892b7186a6d65a9
Silinator <me@silvanfux.ch>
2021-05-22 17:45:13 +0200

    Set vuex states directly in actions instead of over mutations.

--- web/js/vue/store.js
+++ web/js/vue/store.js
@@ -27,20 +27,8 @@ const store = new Vuex.Store({
     setCurrentVideoInfo( state, info ) {
       state.currentVideoInfo = info;
     },
-    setVideosLoading( state, value ) {
-      state.videosLoading = value;
-    },
-    setVideosLoadedIndex( state, value ) {
-      state.videosLoadedIndex = value;
-    },
     addVideos( state, videos ) {
       state.videos = state.videos.concat(videos);
-    },
-    setMoreVideosLoading( state, value ) {
-      state.moreVideosLoading = value;
-    },
-    setMoreVideosLoadedIndex( state, value ) {
-      state.moreVideosLoadedIndex = value;
     },
     addMoreVideos( state, videos ) {
       state.moreVideos = state.moreVideos.concat(videos);
@@ -50,34 +38,34 @@ const store = new Vuex.Store({
   actions: {
     fetchVideos( { commit, state } ) {
       if( state.videosLoading === false ) {
-        commit( 'setVideosLoading', true );
+        state.videosLoading = true;
 
         $.getJSON( 'api/getVideos?index=' + state.videosLoadedIndex, data => {
-          commit( 'setVideosLoading', false );
-          commit( 'setVideosLoadedIndex', data.videos.length );
+          state.videosLoading = false;
+          state.videosLoadedIndex = data.videos.length;
           commit( 'addVideos', data.videos );
 
           if( data.max ) {
-            commit( 'setVideosLoading', 'all' );
+            state.videosLoading = 'all';
           } else {
-            commit( 'setVideosLoading', false );
+            state.videosLoading = false;
           }
         });
       }
     },
     fetchMoreVideos( { commit, state } ) {
       if( state.moreVideosLoading === false ) {
-        commit( 'setMoreVideosLoading', true );
+        state.moreVideosLoading = true;
 
         $.getJSON( 'api/getMoreVideos?index=' + state.moreVideosLoadedIndex + "&filter=" + JSON.stringify(state.moreVideosFilter), data => {
-          commit( 'setMoreVideosLoading', false );
-          commit( 'setMoreVideosLoadedIndex', data.videos.length );
+          state.moreVideosLoading = false;
+          state.moreVideosLoadedIndex = data.videos.length;
           commit( 'addMoreVideos', data.videos );
 
           if( data.max ) {
-            commit( 'setMoreVideosLoading', 'all' );
+            state.moreVideosLoading = 'all';
           } else {
-            commit( 'setMoreVideosLoading', false );
+            state.moreVideosLoading = false;
           }
         });
       }
