dbe9772094a9e9da0a23a21adb2d37efa3bd1172
Silinator <me@silvanfux.ch>
2021-09-26 18:45:52 +0200

    Added cssnano to make minified tailwind css file.

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,6 +1,16 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  }
+const postcssConfig = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer')
+  ]
+};
+
+if (process.env.NODE_ENV === 'production') {
+	postcssConfig.plugins.push(
+		require('cssnano')({
+			preset: 'default',
+		})
+	);
 }
+
+module.exports = postcssConfig;
